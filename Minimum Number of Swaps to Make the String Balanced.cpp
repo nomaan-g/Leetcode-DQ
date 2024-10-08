9aba0fc6906d059431a4737b63dfbc9759e9dfbf
Nomaan gagan <nomaan.gagan@gmail.com>
2024-10-08 23:09:18 +0530

    new commit

---
+++ Minimum Number of Swaps to Make the String Balanced.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int minSwaps(string s) {
+        stack<char> st;
+        
+        for(char &ch : s) {
+            if(ch == '[')
+                st.push(ch);
+            else if(!st.empty())
+                st.pop();
+        }
+        
+        return (st.size()+1)/2;
+    }
+};
